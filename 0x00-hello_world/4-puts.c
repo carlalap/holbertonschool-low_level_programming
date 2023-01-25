9950e8b6ec2572062f6d19ed43ee4088d87ac10a
carlalap <carlos3_alarcon@hotmail.com>
2023-01-25 09:52:44 -0800

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
