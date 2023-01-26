1941150a5ece0396b7eab349c858f9fef4d3a600
carlalap <carlos3_alarcon@hotmail.com>
2023-01-26 08:44:04 -0800

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+
+
+int main(void)
+{
+	n = 0;
+
+	for (n; 10 < n; n++)
+	{
+		printf("%d", n);
+
+	putchar('\n');
+	return (0);
+	}
