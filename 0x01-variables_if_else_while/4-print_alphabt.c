d8acc4459a3714f5e5b7fed4297420554abd57c8
carlalap <carlos3_alarcon@hotmail.com>
2023-01-26 07:50:05 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'q' || letter == 'e')
+	{
+		continue;
+	}
+	putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
