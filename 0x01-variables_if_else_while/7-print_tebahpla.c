f3616763127d2c0086c2c5646135143cbbef71cd
carlalap <carlos3_alarcon@hotmail.com>
2023-01-26 09:55:18 -0800

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* more headers goes there */
+/**
+* main - entry point
+*
+*
+* Return: always return 0
+*
+*/
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter <= 'a' ; letter--)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+	return (0);
+}
