bdbf02fc8558551710c8e6c3541357c62e5b33f2
carlalap <carlos3_alarcon@hotmail.com>
2023-01-26 12:28:07 -0800

    8. Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,14 +12,13 @@
 int main(void)
 {
 
-int num;
-char letter;
+	int num;
+	char letter;
 
-	for (num = '0'; num < '10'; num++)
+	for (num = '0'; num <= '9'; num++)
 		putchar(num);
 
 	for (letter = 'a'; letter <= 'f'; letter++)
-
 	putchar(letter);
 	putchar('\n');
 
