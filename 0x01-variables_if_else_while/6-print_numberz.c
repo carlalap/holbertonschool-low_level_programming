55e67e3f5fe8ccd0139fbb2e0d585a3763ea8bf2
carlalap <carlos3_alarcon@hotmail.com>
2023-01-26 09:51:08 -0800

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: alway returns 0
+*
+*/
+
+int main(void)
+
+{
+	int n = 0;
+
+	for (; n < 10 ; n++)
+		putchar('0' + n);
+	putchar('\n');
+
+	return (0);
+}
