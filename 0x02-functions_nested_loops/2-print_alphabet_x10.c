9c921c9b8253a697c16c758e18399b6bf16ea879
carlalap <carlos3_alarcon@hotmail.com>
2023-01-27 10:25:19 -0800

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,15 +13,15 @@ void print_alphabet_x10(void)
  char letter;
  int num;
 
- for (num = 0; num <= 9; num++)
-
-	 for (letter = 'a'; letter <= 'z'; letter++)
-	 {
-		_putchar(letter);
+	for (num = 0; num <= 9; num++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+ 		{
+			_putchar(letter);
 	 
-	 }
+	 	}	
 
-         _putchar('\n');
- 
+         	_putchar('\n');
+	}
 }
 
