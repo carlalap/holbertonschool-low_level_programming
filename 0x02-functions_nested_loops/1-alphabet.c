825083f540c8b3fb18bd00b4fdfcfcdfa82d127f
carlalap <carlos3_alarcon@hotmail.com>
2023-01-27 09:45:33 -0800

    1. I sometimes suffer from insomnia.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+
+/**
+ * print_alphabet - prints alphabet times
+ *
+ * Return: always 0;
+ *
+ */
+
+void print_alphabet(void);
+
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z' ; letter++)
+	{
+		_putchar(letter);
+	}
+
+	_putchar('\n');
+}
+
