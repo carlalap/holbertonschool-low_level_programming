d72eb479a27b678ad729535f37ae50a44aae8db4
carlalap <carlos3_alarcon@hotmail.com>
2023-01-27 13:04:40 -0800

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,26 +2,25 @@
 
 /**
 * print_sign - main function
-* Returns 1 and prints + if n is greater than zero
-* Returns 0 and prints 0 if n is zero
-* Returns -1 and prints - if n is less than zero
+* @n: character to compare
+* Return: 1 if positive, 0 if zero, -1 if negative
 **/
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
-		return(1);
+		_putchar ('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
-		return(0);
+		_putchar ('0');
+		return (0);
 	}
-	else (n < 0)
+	else
 	{
-		_putchar('-');
-		return(-1);
+		_putchar ('-');
+		return (-1);
 	}
 }
