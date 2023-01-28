5bdd898f212f66798c3183c388ad03793f53a358
carlalap <carlos3_alarcon@hotmail.com>
2023-01-28 14:29:34 -0800

    9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+/**
+ * times_table - times table function
+ *
+ */
+
+void times_table(void)
+
+{
+
+	int a, b, c;
+
+	for (a = 0, a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			int c = a * b;
+
+			if (b == 0)
+			{
+			_putchar('0');
+			}
+			else if (c == 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(c / 10 + '0');
+				_putchar(c % 10 + '0');
+			}
+		}
+		_putchar ('\n');
+	}
+
+
+}
+
