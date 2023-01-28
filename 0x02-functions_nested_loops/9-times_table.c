a660763b2d87912e27c3f929a97afacb5e8502dc
carlalap <carlos3_alarcon@hotmail.com>
2023-01-28 14:36:49 -0800

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,9 +11,9 @@ void times_table(void)
 
 	int a, b, c;
 
-	for (a = 0, a <= 9; a++);
+	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b <= 9; b++);
+		for (b = 0; b <= 9; b++)
 		{
 			int c = a * b;
 
