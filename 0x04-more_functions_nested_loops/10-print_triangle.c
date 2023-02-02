6230418742b8400e9939d65fa2e407f84a4b4108
carlalap <carlos3_alarcon@hotmail.com>
2023-02-01 21:11:05 -0800

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,9 +12,9 @@ void print_triangle(int size)
 	int i, j;
 
 
-	for (i = 1; i <= size; i++)
+	for (i = 0; i <= size; i++)
 	{
-		for (j = 1; j <= size; j++)
+		for (j = 0; j <= size; j++)
 		{
 			if ((i + j) <= size)
 			{
