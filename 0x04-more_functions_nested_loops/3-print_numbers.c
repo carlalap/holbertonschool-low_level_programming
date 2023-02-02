09868f674aef99aeed61fad355456da3a1ce4e20
carlalap <carlos3_alarcon@hotmail.com>
2023-02-01 16:01:54 -0800

    si se puede

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
-* _print_numbers - check the code
-* Description:  prints the numbers, from 0 to 9.
-* Return: Always 0.
+* _print_numbers
+* Description:  prints the numbersfrom 0-9.
 **/
 
 void print_numbers(void)
