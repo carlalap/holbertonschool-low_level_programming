7b4f8e83e5924d9b5bba4c1afcf1a47705147ea3
carlalap <carlos3_alarcon@hotmail.com>
2023-02-03 11:46:01 -0800

    keep walking

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,9 +14,9 @@ void print_rev(char *s)
 	int i;
 
 
-	for (i = _strlen(s); i >= 0; i--)
+	for (i = (_strlen(s) - 1) ; i >= 0; i--)
 	{
-	_putchar(s[i]);
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
