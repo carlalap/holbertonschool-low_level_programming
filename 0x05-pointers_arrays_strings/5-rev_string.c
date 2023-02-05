48755c1aec68b12b83f1b2dc5a67bc66c22d22f9
carlalap <carlos3_alarcon@hotmail.com>
2023-02-05 08:32:29 -0800

    reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include "2-strlen.c"
+
 
 
 /**
@@ -9,26 +9,37 @@
  * loohcS yM
  */
 
-
-
 void rev_string(char *s)
 
 {
-	int i = 0;
-	int count = 0;
-	char len [500];
 
+int i = 0;
+int j = 0;
+char reverse[100];
+
+
+/*count the length of the array num*/
 
 	while (s[i] != '\0')
-
 	{
-	count++;
+		i++;
+	}
+
+/*get the values of num in reverse*/
+
+	for (; i > 0; i--)
+	{
+		reverse[j] = s[i - 1];
+		j++;
+	}
+
+/*puts back the reverse values in num again*/
+
+	while (i < j)
+	{
+	s[i] = reverse[i];
 	i++;
 	}
 
-	for (; i > 0; i++)
-	{
-	_putchar(s[i]);
-	}
+}
 
-	
