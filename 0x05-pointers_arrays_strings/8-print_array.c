b7b7e077b63b3ae4ce8d14d4aa7ed6d682d1358a
carlalap <carlos3_alarcon@hotmail.com>
2023-02-04 18:19:10 -0800

    keep walking

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,12 +12,12 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < n; i++)
-	 {
-	  printf("%d", a[i]);
+	{
+	printf("%d", a[i]);
 
 
-		if (i != n-1)
-		  printf(", ");
-	 }
+		if (i != n - 1)
+		printf(", ");
+	}
 printf("\n");
 }
