97c6b5b7cc387bfae976727f175decd3f2abd996
carlalap <carlos3_alarcon@hotmail.com>
2023-02-04 18:07:26 -0800

    keep walking

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,9 +4,7 @@
  * print_array - prints array
  * @a: pointer of array
  * @n: how many times to prints
- *
- */
-
+*/
 
 void print_array(int *a, int n)
 
@@ -14,8 +12,13 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < n; i++)
-	 {
-	  printf ("%d" ", ", a[i]);
-	 }
-	printf ("\n");	
+	{
+	printf("%d", a[i]);
+
+		if (i != n - 1)
+		printf("' ");
+
+	}
+
+printf("\n");
 }
