2ee4235e575a6444217c9c0d3f1c6f49712b3d99
carlalap <carlos3_alarcon@hotmail.com>
2023-02-09 08:44:17 -0800

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+
+/**
+* _strcmp - string to compare
+* @s1: first string
+* @s2: second string
+* Return: value of string
+*/
+
+
+
+int _strcmp(char *s1, char *s2)
+{
+
+	int i;
+	int j;
+
+	for (i = 0; (s1[i] - s2[i]) != '\0' || s1[i] != '\0'; i++)
+
+	{
+		j = s1[i] - s2[i];
+	}
+
+	return (j);
+}
+
