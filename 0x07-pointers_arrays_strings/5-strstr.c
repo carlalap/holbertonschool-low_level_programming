5a81a5e6d74e3e6fc01d91185d6a86fb726cb811
carlalap <carlos3_alarcon@hotmail.com>
2023-02-12 20:40:59 -0800

    5. strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,10 +5,9 @@
  * _strstr - Locates a substring.
  * @haystack: The string to be searched.
  * @needle: The substring to be located.
- *
  * Return: If the substring is located - a pointer to the beginning
- *                                       of the located substring.
- *         If the substring is not located - NULL.
+ * of the located substring.
+ * If the substring is not located - NULL.
  */
 
 char *_strstr(char *haystack, char *needle)
