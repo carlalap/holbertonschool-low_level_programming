5dbfe918ae9f464b28088e47e1ed856361138322
carlalap <carlos3_alarcon@hotmail.com>
2023-02-15 09:10:11 -0800

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+/**
+ * _puts_recursion - print string
+ * @s: string to print
+ * Description: print string using recursion
+ * Return: nothing
+ **/
+
+void _puts_recursion(char *s)
+
+
+{
+
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+
+	{
+		_putchar(s[i]);
+		_puts_recursion(&s[i + 1]);
+	}
+
+	_putchar('\n');
+
+}
