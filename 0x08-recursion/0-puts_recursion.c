994a245743c5f2cbc6703e65f737f8f81b007f55
carlalap <carlos3_alarcon@hotmail.com>
2023-02-15 09:30:23 -0800

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -17,13 +17,15 @@ void _puts_recursion(char *s)
 
 	i = 0;
 
-	while (s[i] != '\0')
+	if (s[i] != '\0')
 
 	{
 		_putchar(s[i]);
 		_puts_recursion(&s[i + 1]);
 	}
 
+	if (s[i] == '\0')
+	{
 	_putchar('\n');
-
+	}
 }
