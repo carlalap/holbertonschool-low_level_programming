ce8b5e37fbbc3c0405aa617a886354bb0ecf70e3
carlalap <carlos3_alarcon@hotmail.com>
2023-02-17 08:59:46 -0800

    0.prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * main - program that prints name of self
+ * @argc: int num of inbound args
+ * @argv: pointer to array of pointers w/ said args
+ * Return: Always 0 if it pass
+ */
+
+
+int main(int argc, char *argv[])
+
+{
+	int i = 0;
+
+	if (argc)
+	{}
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
