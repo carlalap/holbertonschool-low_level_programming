7f60e372f11ee84fdc325e85011bfe62fc75dd41
carlalap <carlos3_alarcon@hotmail.com>
2023-02-17 11:50:50 -0800

    multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program that prints result of multiplication
+ * @argc: int num of inbound args
+ * @argv: pointer to array of pointers w/ said args
+ * Description: print resul of mul of 2 arguments
+ * Return: if is less 3 arg prints Error
+ */
+
+int main (int argc, char *argv[])
+
+{
+	int result;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	result = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", result);
+	return (0);
+
+}
