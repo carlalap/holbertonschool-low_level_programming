bfb2c3e5d8fbeb2c0a102105ada1609d46a36d77
carlalap <carlos3_alarcon@hotmail.com>
2023-02-24 12:28:09 -0800

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,24 +17,21 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *s;
 
 	/* If nmemb or size is 0, _calloc returns NULL */
-        if (nmemb == 0 || size == 0)
-                return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-        /*declaration malloc*/
-        s = (char *)malloc(nmemb * size);
+	/*declaration malloc*/
+	s = (char *)malloc(nmemb * size);
 
-        /*if malloc fails, _calloc returns NULL*/
-        if (s == NULL)
-                return (NULL);
+	/*if malloc fails, _calloc returns NULL*/
+	if (s == NULL)
+		return (NULL);
 
-        /*allocation memory for array*/
-        for (i = 0; i < nmemb * size; i++)
-                s[i] = 0;
+	/*allocation memory for array*/
+	for (i = 0; i < nmemb * size; i++)
+		s[i] = 0;
 
-        return (s);
-
-
-
+	return (s);
 }
 
 
