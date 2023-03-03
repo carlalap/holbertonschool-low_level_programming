5ebeef07f3da1e9d0d27f0f3e1cd43a63f3f2891
carlalap <carlos3_alarcon@hotmail.com>
2023-03-02 16:24:15 -0800

    5.free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_dog - frees dog
+ * @d: dog to free
+ */
+
+void free_dog(dog_t *d)
+
+{
+
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+
+}
