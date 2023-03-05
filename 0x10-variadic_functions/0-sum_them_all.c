2da39dda4567d931b6856f41386a362b0e5f10d7
carlalap <carlos3_alarcon@hotmail.com>
2023-03-04 19:11:01 -0800

    1. To be is to be the value of a variable

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,7 +17,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(ap, int);
+		sum = sum + va_arg(ap, int);
 	}
 	va_end(ap);
 	return (sum);
