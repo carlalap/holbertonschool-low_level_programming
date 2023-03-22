de3ae500a9ece8c7db2be762a2021c326ea6d24c
carlalap <carlos3_alarcon@hotmail.com>
2023-03-22 09:52:06 -0700

    0. Print list

---
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints length and elements
+ * @h: head of list
+ * Return: size_t value
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+
+{
+	const dlistint_t *tmp;
+	int count = 0;
+
+	tmp = h;
+	
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->);
+		tmp = tmp->next;
+		count++;
+	}
+	return(count);
+}
