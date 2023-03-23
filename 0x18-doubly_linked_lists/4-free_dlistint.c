693a48d32e8ed84af9e1be55cb8a1a954f756b36
carlalap <carlos3_alarcon@hotmail.com>
2023-03-23 09:20:17 -0700

    4. Free list

--- 0x18-doubly_linked_lists/4-free_dlistint.c
+++ 0x18-doubly_linked_lists/4-free_dlistint.c
@@ -3,18 +3,16 @@
 /**
  * free_dlistint - Frees the double  dlistint_t dlinked list from memory
  * @head: The head of the doubly linked list
- * Return: void
  */
 
 
 void free_dlistint(dlistint_t *head)
-
 {
 	dlistint_t *temp;
 
 	temp = head;
 
-	if (temp != NULL)
+	while (temp != NULL)
 	{
 		free(temp);
 		temp = temp->next;
