e5c9ae433d7e125e4e1627f884cf450d705a0907
carlalap <carlos3_alarcon@hotmail.com>
2023-12-06 22:59:57 +0000

    task0. betty

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,10 +1,9 @@
 #include "search_algos.h"
-
 /**
  * linear_search - a function that searches for a value in an array
- *@array: the array to search in
- *@size: number of elements in array
- *@value: is the value to search for
+ * @array: the array to search in
+ * @size: number of elements in array
+ * @value: is the value to search for
  *
  * Return - return the value searched or -1 if not or if array is NULL 
  */
@@ -16,14 +15,13 @@ int linear_search(int *array, size_t size, int value)
         return -1;
     }
 	for (n = 0; n < size; n++)
-    
+
 	{
 		printf("Value checked array[%lu] = [%d]\n", n, array[n]);
         if (array[n] == value)
         {
 			return (n);
         }
-        
 	}
 	return (-1);
 }
