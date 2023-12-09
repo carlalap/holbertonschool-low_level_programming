c506e89ff701d96881f762112890de6e385bc4b0
carlalap <carlos3_alarcon@hotmail.com>
2023-12-09 19:26:02 +0000

    betty fixed task.1

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -4,25 +4,28 @@
 /**
  * print_array - prints an array
  * @array: array to search
- * @start: pointer to the start of array
- * @start: pointer to the end ofarray
+ * @left: pointer to the start of array
+ * @right: pointer to the end ofarray
  * Return: array in specific way
 */
 
-void print_array(int *array, int left, int right) {
-    int i;
+void print_array(int *array, int left, int right)
+{
+	int i;
 
-    printf("Searching in array: ");
-    for (i = left; i <= right; i++) {
-        printf("%d", array[i]);
-        if (i < right)
-            printf(", ");
-    }
-    printf("\n");
+	printf("Searching in array: ");
+
+	for (i = left; i <= right; i++)
+	{
+		printf("%d", array[i]);
+		if (i < right)
+			printf(", ");
+	}
+	printf("\n");
 }
 
 /**
- * binary_search - binary search algorithm for a value in a sorted array 
+ * binary_search - binary search algorithm for a value in a sorted array
  * @array: array to search
  * @size: dimension/size of array
  * @value: value to search of array
@@ -30,26 +33,30 @@ void print_array(int *array, int left, int right) {
  *         or -1 if not or if array is NULL
 */
 
-int binary_search(int *array, size_t size, int value) {
-    
-    int left, right, middle;
-    
-    if (array == NULL)
-        return -1;
+int binary_search(int *array, size_t size, int value)
+{
+	int left, right, middle;
 
-    left = 0;
-    right = size - 1;
- 
-    while (left <= right) {
-        print_array(array, left, right);
-        middle = left + (right - left) / 2;
-        if (array[middle] == value)
-            return middle;
-        else if (array[middle] < value)
-            left = middle + 1;
-        else
-            right = middle - 1;
-    }
+	if (array == NULL)
+		return (-1);
 
-    return -1;
+	left = 0;
+	right = size - 1;
+
+	while (left <= right)
+	{
+		print_array(array, left, right);
+		middle = left + (right - left) / 2;
+
+		if (array[middle] == value)
+			return (middle);
+
+		else if (array[middle] < value)
+			left = middle + 1;
+		else
+			right = middle - 1;
+	}
+	return (-1);
+
 }
+
